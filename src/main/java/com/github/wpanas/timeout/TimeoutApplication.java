76f5a5a947cb3e39d7464a50f0fe2f7c98889c37
wpanas <wpanas@users.noreply.github.com>
2020-03-24 12:41:11 +0100

    Init

---
+++ src/main/java/com/github/wpanas/timeout/TimeoutApplication.java
@@ -0,0 +1,13 @@
+package com.github.wpanas.timeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wpanas/timeout/TimeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wpanas.timeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
